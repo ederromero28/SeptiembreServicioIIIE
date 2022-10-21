3b1b9ff553da116750735b6f45740e567a225a34
Eder Romero <ederromero32@gmail.com>
2022-10-21 14:28:14 -0500

    cambios de seguridad con jwt

--- src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
+++ src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
@@ -1,19 +1,24 @@
 package com.idat.SetiembreIIIE.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 //import org.springframework.security.core.userdetails.User;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,29 +26,67 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
-
+    
+    @Autowired
+    private UserDetailService service;
+    
+    @Autowired
+    private TokenFilter filter;
+    
+    @Autowired
+    private EntryPoint entryPoint;
+    
 	@Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // TODO Auto-generated method stub
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        // TODO Auto-generated method stub
+        return super.authenticationManager();
+    }
+
+    @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("admin")
-		.password(encriptado().encode("root"))
-		.roles("ADMIN");
+//		auth.inMemoryAuthentication()
+//		.withUser("admin")
+//		.password(encriptado().encode("root"))
+//		.roles("ADMIN");
 		//
-		auth.inMemoryAuthentication()
-		.withUser("usuario01")
-		.password(encriptado().encode("123"))
-		.roles("USUARIO");
+//		auth.inMemoryAuthentication()
+//		.withUser("usuario01")
+//		.password(encriptado().encode("123"))
+//		.roles("USUARIO");
+        
+        auth.userDetailsService(service).passwordEncoder(encriptado());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET).permitAll()
-			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
-			.and()
-			.httpBasic()
-			.and()
-			.csrf().disable();
+//		http.authorizeRequests()
+//			.antMatchers(HttpMethod.GET).permitAll()
+//			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.csrf().disable();
+	    
+	    http.authorizeRequests()
+	        .antMatchers("/crearToken").permitAll()
+	        .anyRequest()
+	        .authenticated()
+	        .and()
+	        .exceptionHandling()
+	        .authenticationEntryPoint(entryPoint)
+	        .and()
+	        .sessionManagement()
+	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        .and()
+	        .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+	        .csrf().disable();
+	        
 	}
 	
 	
---
+++ src/main/java/com/idat/SetiembreIIIE/security/UserDetailService.java
@@ -0,0 +1,25 @@
+package com.idat.SetiembreIIIE.security;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        if ("root".equals(username)) {
+            return new User("root", "12345", new ArrayList<>());
+        } else {
+            throw new  UsernameNotFoundException("Usuario no existe " + username);
+        }
+        
+    }
+
+}
