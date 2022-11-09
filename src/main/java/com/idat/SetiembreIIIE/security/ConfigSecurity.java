ee86b2d237da728a186351508c5399ba5917b1a4
Eder Romero <ederromero32@gmail.com>
2022-11-09 10:51:41 -0500

    commit clase semana 8 oauth2

--- src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
+++ src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
@@ -3,45 +3,32 @@ package com.idat.SetiembreIIIE.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
-    
+
     @Autowired
     private UserDetailService service;
-    
-    @Autowired
-    private TokenFilter filter;
-    
-    @Autowired
-    private EntryPoint entryPoint;
-    
-	@Override
+
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         // TODO Auto-generated method stub
         return super.authenticationManagerBean();
     }
 
+    @Bean
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         // TODO Auto-generated method stub
@@ -49,80 +36,26 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
-//		.withUser("admin")
-//		.password(encriptado().encode("root"))
-//		.roles("ADMIN");
-		//
-//		auth.inMemoryAuthentication()
-//		.withUser("usuario01")
-//		.password(encriptado().encode("123"))
-//		.roles("USUARIO");
-        
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.userDetailsService(service).passwordEncoder(encriptado());
-	}
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//			.antMatchers(HttpMethod.GET).permitAll()
-//			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
-//			.and()
-//			.httpBasic()
-//			.and()
-//			.csrf().disable();
-	    
-	    http.authorizeRequests()
-	        .antMatchers("/crearToken").permitAll()
-	        .anyRequest()
-	        .authenticated()
-	        .and()
-	        .exceptionHandling()
-	        .authenticationEntryPoint(entryPoint)
-	        .and()
-	        .sessionManagement()
-	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	        .and()
-	        .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-	        .csrf().disable();
-	        
-	}
-	
-	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-	// METODO
-//	@Bean
-//	public UserDetailsService userDetail() {
-//
-//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//		manager.createUser(
-//					User.withUsername("eder")
-//					.password(encriptado().encode("123"))
-//					.roles("ADMIN")
-//					.build());
-//		return manager;
-//	}
-//	
-	@Bean
-	public PasswordEncoder encriptado() {
-		return new BCryptPasswordEncoder();
-	}
-//	
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		
-//		http.authorizeRequests()
-//			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
-//			.and()
-//			.httpBasic()
-//			.and()
-//			.csrf().disable();
-//		
-//		return http.build();
-//	}
-	
-	
-	
-	
+        http.anonymous().disable();
+
+    }
+
+    @Bean
+    public PasswordEncoder encriptado() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
 }
